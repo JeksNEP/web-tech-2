12c8b9179504fbc1893a38221813611e5b470a5c
JeksNEP <nepnepu@mail.ru>
2024-09-26 04:29:04 +0300

    first

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+from databases import Database
+import sqlalchemy
+
+DATABASE_URL = "postgresql://user:1234@localhost:5423/database"
+
+database = Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
 
 app = FastAPI()
 
